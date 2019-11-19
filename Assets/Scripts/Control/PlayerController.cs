34d25298688f4b2e708e36835d4f5ae51580b5a6
Dnyberg <48476448+Dnyberg@users.noreply.github.com>
2019-11-19 15:59:32 +0100

    Lecture 29 done

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
+using System;
 
 namespace RPG.Control
 {
@@ -10,23 +12,50 @@ namespace RPG.Control
 
         private void Update()
         {
-            if (Input.GetMouseButton(0))
-            {
-                MoveToCursor();
-            }
+            if(InteractWithCombat()) return;
+            if(InteractWithMovement()) return;
+
         }
-        void MoveToCursor()
+
+        private bool InteractWithCombat()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+
+                if(Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+                return true;
+
+            }
+            return false;
+        }
+
+        private bool InteractWithMovement()
+        {
+
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast((Ray)GetMouseRay(), out hit);
 
             if (hasHit)
             {
-                GetComponent<Mover>().MoveTo(hit.point);
+                if (Input.GetMouseButton(0))
+                {
+                     GetComponent<Mover>().StartMoveAction(hit.point);
+                }
+                return true;
 
             }
+            return false;
 
+        }
+        private static Ray GetMouseRay()
+        {
+             return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
